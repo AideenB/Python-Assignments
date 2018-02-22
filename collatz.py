3853d958ac78617bb0bf088f6096f4e13b08c988
{name} <{emailaddress}>
2018-02-22 22:47:14 +0000

    update

--- collatz.py
+++ collatz.py
@@ -4,11 +4,11 @@
 
 num = 17
  
-while num != 1:
+while num != 1: #num not equal to 1
     print(num)
-    if num % 2 == 0:
-      num = int(num / 2)
-    else:
-      num = int(3 * num + 1)
+    if num % 2 == 0: #even number
+      num = int(num / 2) #if even divide by 2
+    else: #if not an even number it must be odd!
+      num = int(3 * num + 1) #multiply by 3 and add 1
 else:
     print(num)
---
+++ euler5.py
@@ -0,0 +1,12 @@
+#Aideen Byrne 20/02/2018
+#Project Euler Problem 5: https://projecteuler.net/problem=5 
+#Demonstrate the smallest positive number that is evenly divisible by all the numbers from  to 20
+
+i = 2 #all numbers divisible by 1 so start with next integer
+for k in (range(11, 21)):#range from 11 to 20
+  if i % k > 0: 
+    for j in range(11, 21): 
+      if (i*j) % k == 0: #even numbers only
+        i *= j 
+        break 
+print (i)
