4a15a064cc62ebf2010c388be4df6591ab947755
AideenByrne <g00364780@gmit.ie>
2018-04-02 11:21:35 +0100

    updated factorial.py

--- factorial.py
+++ factorial.py
@@ -14,6 +14,7 @@ def factorial(y): #to define function
 
 print("The factorial of 5 is: ",factorial(5))
 print("The factorial of 7 is: ",factorial(7))
+print ("The factorial of 10 is: ",factorial(10))
 
 
 
